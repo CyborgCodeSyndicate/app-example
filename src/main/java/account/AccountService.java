9e28c770f3863a965d7e66df35d38ddd5073cd8e
CyborgFrodo <frodo@cyborgcode.io>
2025-08-12 20:26:40 +0200

    Add change to trigger tagged tests upon deploy simulation 2

--- src/main/java/account/AccountService.java
+++ src/main/java/account/AccountService.java
@@ -2,7 +2,7 @@ package account;
 
 public class AccountService {
     public String getAccountStatus(String id) {
-        System.out.println("tested");
+        System.out.println("test 1");
         return "ACTIVE";
     }
 }
