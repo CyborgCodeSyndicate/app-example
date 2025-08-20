05bf346480e3ca6e9075cb4851607e77f57bff5c
CyborgFrodo <frodo@cyborgcode.io>
2025-08-20 20:34:36 +0200

    Trigger tagged tests upon deploy

--- src/main/java/account/AccountService.java
+++ src/main/java/account/AccountService.java
@@ -2,7 +2,6 @@ package account;
 
 public class AccountService {
     public String getAccountStatus(String id) {
-        System.out.println("Introducing change in AccountService");
         return "ACTIVE";
     }
 }
