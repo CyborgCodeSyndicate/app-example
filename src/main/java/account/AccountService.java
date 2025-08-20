00adbf71a33145f8d27022663b7c291178551d47
CyborgFrodo <frodo@cyborgcode.io>
2025-08-20 18:31:15 +0200

    Trigger tagged tests upon deploy

--- src/main/java/account/AccountService.java
+++ src/main/java/account/AccountService.java
@@ -2,6 +2,7 @@ package account;
 
 public class AccountService {
     public String getAccountStatus(String id) {
+        System.out.println("Introducing change in AccountService");
         return "ACTIVE";
     }
 }
--- src/main/java/payment/PaymentService.java
+++ src/main/java/payment/PaymentService.java
@@ -2,7 +2,7 @@ package payment;
 
 public class PaymentService {
     public boolean processPayment(String from, String to, double amount) {
-
+        System.out.println("Introducing change in PaymentService");
         return true;
     }
 }
